e6707e8f81eab5771126a84902348dbff26bea3b
Leonard Tye <leonardtye1992@gmail.com>
2018-04-13 07:30:33 +0800

    Everything works!

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require("express");
-const app = express();
 const route = require("./server/routes/router");
 const mongo = require("mongodb").MongoClient;
 const dotenv = require("dotenv").config();
 const uri = process.env.MONGOLAB_URI;
 
+const app = express();
+
 mongo.connect(uri, (err, db) => {
-	if (err) {
-		console.log("Unable to connect to server", err);
-	} else {
-		console.log("connected");
-		route(app, db);
-	}
+	if (err) throw err;
+	route(app, db);
 });
 
 app.listen(process.env.PORT || 3000); // Must use process.env.PORT because heroku and other cloud hosts may not use port 3000
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,34 +1,32 @@
 const express = require("express");
 const path = require("path");
-const router = express.Router();
 const request = require("request");
+
 module.exports = (app, db) => { // Must pass app in as argument and not just define in global scope
 
 	const database = db.db("image-search-abstraction-layer-fcc"),
 		collection = database.collection("images");
+
 	app.get("/search/:search", (req, res) => {
-		const url = `https://www.googleapis.com/customsearch/v1?key=AIzaSyCqlMaAya6CnhDtIvnZTeGIUNWKKLHPMo0&searchType=image&fields=items(htmlTitle,link,snippet,image/contextLink,image/thumbnailLink)&prettyPrint=false&cx=${process.env.SEARCHID}&q=`
-		var startIndex = 1;
-		var searchTerm = req.params.query;
-		var date = new Date();
+		const url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyCqlMaAya6CnhDtIvnZTeGIUNWKKLHPMo0&searchType=image&fields=items(htmlTitle,link,snippet,image/contextLink,image/thumbnailLink)&cx=000561780369073625013:42m_h7r79sy&q=",
+			term = req.params.search,
+			date = new Date();
+		let index = 1;
 
 		if (req.query.offset) {
-			startIndex = req.query.offset * 10
-			console.log(startIndex)
+			index = req.query.offset * 10;
+			if (index > 10) index = 1;
 		}
-		request(`${url}${searchTerm}&start=${startIndex}`, function(error, response, body) {
-			var data = JSON.parse(body)
-			if (error) {
-				console.log('error', error); //print Error
-				res.send(error)
-			}
+		request(`${url}${term}&start=${index}`, (err, response, body) => {
+			const data = JSON.parse(body);
+			if (err) throw err;
 			if (response.statusCode === 200) {
 				res.send(data)
 			}
 		})
 
 		collection.insert({
-			"term": searchTerm,
+			"term": term,
 			"when": date
 		})
 
@@ -36,15 +34,12 @@ module.exports = (app, db) => { // Must pass app in as argument and not just def
 
 
 	app.get("/latest", (req, res) => {
+		collection.find().toArray((err, data) => {
+			if (err) throw err;
+			res.send(data);
+		});
+	});
 
-			collection.find().toArray(function(err, items) {
-				if (err) throw err
-
-				res.send(items)
-			})
-		}
-
-	);
 	app.get("/api", (req, res) => { // NOTE: Name of route must be defined in webpack config under proxy "/api" if using dev server
 		res.status(200).send(req.protocol + '://' + req.get("host"));
 	});
@@ -56,4 +51,4 @@ module.exports = (app, db) => { // Must pass app in as argument and not just def
 	});
 
 
-}
+};
